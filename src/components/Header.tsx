e517b7c4a39864762671c1a1db88af70fb93bc8b
klokke <esinto8@protonmail.com>
2022-09-19 21:34:14 +0100

    second commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import SunIcon from './Icons/SunIcon'
-import MoonIcon from './Icons/MoonIcon'
-import avatar from '../assets/avatar.jpeg'
-import '../css/skeleton.css'
-import '../css/normalize.css'
-import '../css/components.css'
-import { ThemeType } from '../ts/types';
+import React from "react";
+import SunIcon from "./Icons/SunIcon";
+import MoonIcon from "./Icons/MoonIcon";
+import avatar from "../assets/avatar.jpeg";
+import "../css/skeleton.css";
+import "../css/normalize.css";
+import "../css/components.css";
+import { ThemeType } from "../ts/types";
 
 function Header(): JSX.Element {
-  const dataTheme = document.body.getAttribute('data-theme');
-  const [theme, setTheme] = React.useState<ThemeType>((): ThemeType => dataTheme === "light" ? "light" : 'dark');
+  const dataTheme = document.body.getAttribute("data-theme");
+  const [theme, setTheme] = React.useState<ThemeType>(
+    (): ThemeType => (dataTheme === "light" ? "light" : "dark")
+  );
 
   React.useEffect(() => {
-    document.body.setAttribute('data-theme', theme)
+    document.body.setAttribute("data-theme", theme);
   }, [theme]);
 
   const handleSwitchTheme = () => {
-    setTheme(isDark ? 'light' : 'dark')
-  }
+    setTheme(isDark ? "light" : "dark");
+  };
 
-  const isDark = theme === 'dark'
+  const isDark = theme === "dark";
 
   return (
     <div className="Header container">
@@ -27,14 +29,14 @@ function Header(): JSX.Element {
         <img src={avatar} alt="avatar" />
         &nbsp;&nbsp;&nbsp;
         <h2>
-          <b>Sagnik. </b>
+          <b>Food Lover. </b>
         </h2>
       </div>
       <button className="switch-theme-button" onClick={handleSwitchTheme}>
-        {isDark ? <SunIcon color="white" /> : <MoonIcon  />}
+        {isDark ? <SunIcon color="white" /> : <MoonIcon />}
       </button>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
